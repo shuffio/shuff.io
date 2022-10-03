fcd1ee666c0d2d5fff7b544f53b3acb4c1cc9591
Adam Fratino <adam.fratino@onepeloton.com>
2022-10-03 12:58:34 -0400

    nextjs init

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>shuff.io</title>
+        <meta
+          name="description"
+          content="your one-stop shop for hot shuff gossip"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>nav</nav>
+      </header>
+
+      <main>main</main>
+
+      <footer>footer</footer>
+    </>
+  );
+};
+
+export default Home;
