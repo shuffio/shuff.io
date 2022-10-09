917506959630ef70dbb0dcd55cec4187ae1e0206
Adam Fratino <adam.fratino@onepeloton.com>
2022-10-09 14:33:54 -0400

    revert global default font

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -6,10 +6,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    font-family: ${families.serif};
-    /* font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, Segoe UI,
-      Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-      sans-serif; */
+    font-family: ${families.sans};
   }
 
   a { color: inherit; }
--- ui/tokens/fonts.ts
+++ ui/tokens/fonts.ts
@@ -3,7 +3,9 @@
  */
 
 export const families = {
-  sans: "Source Sans Pro, sans-serif",
+  sans: `"Source Sans Pro", -apple-system, BlinkMacSystemFont, Segoe UI,
+  Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+  sans-serif`,
   serif: "Merriweather, serif",
   mono: "monospace",
 };
